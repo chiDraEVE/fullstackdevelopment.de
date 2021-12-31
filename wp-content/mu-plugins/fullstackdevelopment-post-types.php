bab8890c8669cf9183fabf5027b547593487e36e
chidraeve <lukas.mainka@clubitsolutions.de>
2021-12-31 20:04:33 +0100

    added taxonomy udemy-course and linked it to course, page and post

--- wp-content/mu-plugins/fullstackdevelopment-post-types.php
+++ wp-content/mu-plugins/fullstackdevelopment-post-types.php
@@ -1,13 +1,13 @@
 <?php
 	
-	add_action('init', create_course_cpt);
+	add_action('init', 'fullstackdevelopment_create_course_cpt');
 	
-	function create_course_cpt() {
+	function fullstackdevelopment_create_course_cpt() {
 		$args = array(
 			'public' => true,
-			'label' => 'Kurse',
+			'label' => 'course',
 			'has_archive' => true,
-			'rewrite' => array( 'slug' => 'kurs'),
+			'rewrite' => array( 'slug' => 'course'),
 			'supports' => array(
 				'title',
 				'editor',
@@ -20,4 +20,22 @@
 		);
 		register_post_type('course', $args);
 	}
+	
+	add_action('init', 'fullstackdevelopment_create_udemy_course_taxonomy');
+	function fullstackdevelopment_create_udemy_course_taxonomy() {
+		register_taxonomy( 'udemy-course', array( 'page', 'post', 'course' ), array(
+			'hierarchical' => false,
+			'label' => 'udemy-course',
+			'show_ui' => true,
+			'show_in_rest' => true,
+			'show_admin_column' => true,
+			'query_var' => true,
+			'rewrite' => array('slug' => 'udemy-course')
+		) );
+	}
 
+	add_action('init', 'fullstackdevelopment_add_taxonomies_to_courses');
+	function fullstackdevelopment_add_taxonomies_to_courses() {
+		register_taxonomy_for_object_type( 'category', 'course' );
+		register_taxonomy_for_object_type( 'post_tag', 'course' );
+	}
