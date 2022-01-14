71e19327f9e77f13168fa91ce3de99f507341cee
chidraeve <lukas.mainka@clubitsolutions.de>
2022-01-14 19:45:14 +0100

    custom-post-types and taxonomies are ready to go, also added the relations between the post-types. There's still custom fields to create tough

--- wp-content/mu-plugins/fullstackdevelopment-post-types.php
+++ wp-content/mu-plugins/fullstackdevelopment-post-types.php
@@ -1,13 +1,11 @@
 <?php
 	
-	add_action('init', 'fullstackdevelopment_create_course_cpt');
+	add_action('init', 'fullstackdevelopment_post_types');
 	
-	function fullstackdevelopment_create_course_cpt() {
-		$args = array(
+	function fullstackdevelopment_post_types() {
+		$common = array(
 			'public' => true,
-			'label' => 'course',
 			'has_archive' => true,
-			'rewrite' => array( 'slug' => 'course'),
 			'supports' => array(
 				'title',
 				'editor',
@@ -18,29 +16,94 @@
 				'revision'
 			)
 		);
-		register_post_type('course', $args);
 		
-		$args['label'] = 'project';
-		$args['rewrite'] = array('slug' => 'project');
+		$sourceArguments = array(
+			'label' => 'source',
+			'rewrite' => array( 'slug' => 'source'),
+			'menu_icon' => 'dashicons-feedback',
+			'labels' => array(
+				'name' => 'Quelle',
+				'add_new_item' => 'Neue Quelle hinzufügen',
+				'edit_item' => 'Quelle bearbeiten',
+				'all_items' => 'Alle Quellen',
+				'singular_name' => 'Quelle'
+			),
+			'menu_position' => 40
+		);
 		
-		register_post_type( 'project', $args );
+		register_post_type('source', array_merge($common, $sourceArguments));
+		
+		$projectArguments = array(
+			'label' => 'project',
+			'rewrite' => array( 'slug' => 'project'),
+			'menu_icon' => 'dashicons-portfolio',
+			'labels' => array(
+				'name' => 'Projekte',
+				'add_new_item' => 'Neues Projekt anlegen',
+				'edit_item' => 'Projekt bearbeiten',
+				'all_items' => 'Alle Projekte',
+				'singular_name' => 'Projekt'
+			),
+			'menu_position' => 30
+		);
+		
+		register_post_type( 'project', array_merge($common, $projectArguments) );
+		
+		$authorArguments = array(
+			'label' => 'author',
+			'rewrite' => array( 'slug' => 'author'),
+			'menu_icon' => 'dashicons-id',
+			'labels' => array(
+				'name' => 'Authoren',
+				'add_new_item' => 'Neuen Author anlegen',
+				'edit_item' => 'Author bearbeiten',
+				'all_items' => 'Alle Authoren',
+				'singular_name' => 'Author'
+			),
+			'menu_position' => 50
+		);
+		
+		register_post_type( 'author', array_merge($common, $authorArguments) );
 	}
 	
-	add_action('init', 'fullstackdevelopment_create_udemy_course_taxonomy');
-	function fullstackdevelopment_create_udemy_course_taxonomy() {
-		register_taxonomy( 'udemy-course', array( 'page', 'post', 'course', 'project' ), array(
+	add_action('init', 'fullstackdevelopment_taxonomies');
+	function fullstackdevelopment_taxonomies() {
+		register_taxonomy( 'vendor', array( 'project' ), array(
 			'hierarchical' => false,
-			'label' => 'udemy-course',
+			'label' => 'vendor',
 			'show_ui' => true,
 			'show_in_rest' => true,
 			'show_admin_column' => true,
 			'query_var' => true,
-			'rewrite' => array('slug' => 'udemy-course')
+			'rewrite' => array('slug' => 'vendor')
+		) );
+		
+		register_taxonomy( 'project-type', array( 'project', 'source' ), array(
+			'hierarchical' => true,
+			'labels' => array(
+				'name' => 'Projektarten',
+				'singular_name' => 'Projektart',
+				'menu_name' => 'Projektart',
+				'all_items' => 'Alle Projektarten',
+				'edit_item' => 'Projektart bearbeiten',
+				'view_item' => 'Projektart anschauen',
+				'update_item' => 'Projektart aktualisieren',
+				'add_new_item' => 'Neue Projektart erstellen',
+				'parent_item' => 'Übergeordnete Projektart',
+				'search_items' => 'Projektart suchen'
+			),
+			'show_ui' => true,
+			'show_in_rest' => true,
+			'show_admin_column' => true,
+			'query_var' => true,
+			'rewrite' => array('slug' => 'project-type')
 		) );
 	}
 
 	add_action('init', 'fullstackdevelopment_add_taxonomies_to_courses');
 	function fullstackdevelopment_add_taxonomies_to_courses() {
-		register_taxonomy_for_object_type( 'category', 'course' );
-		register_taxonomy_for_object_type( 'post_tag', 'course' );
+		register_taxonomy_for_object_type( 'category', 'source' );
+		register_taxonomy_for_object_type( 'category', 'project' );
+		register_taxonomy_for_object_type( 'post_tag', 'source' );
+		register_taxonomy_for_object_type( 'post_tag', 'project' );
 	}
