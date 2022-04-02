e18e50ce1cbc090f8ca790d528f3283fa99a2eab
chidraeve <lukas.mainka@clubitsolutions.de>
2022-04-02 13:20:43 +0200

    Integrated Natours Project into Project

--- wp-content/themes/clubitsolutions-child_theme/functions.php
+++ wp-content/themes/clubitsolutions-child_theme/functions.php
@@ -23,7 +23,6 @@ function clubitsolutions_theme_parent_theme_enqueue_styles() {
 		array( 'clubitsolutions-theme-style' )
 	);
 	
-	
 	if ( is_page( 'fictional-university' ) ) {
 		wp_enqueue_style( 'fictional-university-style', get_stylesheet_directory_uri() . '/assets/fictional-university/styles.css' );
 		wp_enqueue_script('fictional-university-scripts', get_stylesheet_directory_uri() . '/assets/fictional-university/scripts.js', array('jquery'), '1.0', true);
@@ -33,17 +32,35 @@ function clubitsolutions_theme_parent_theme_enqueue_styles() {
 	}
 	
 	if ( get_post_type() == 'project') {
+		$currentlyDeveloping = array(
+			'natours' => true,
+			'nexter' => false,
+		);
+		
 		if ( get_post_field( 'post_name', get_post()) == 'nexter') {
 			
 			wp_enqueue_style('josefin-sans', '//fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;1,400');
 			wp_enqueue_style('nunito', '//fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;1,400');
 			
-			if (strstr($_SERVER['SERVER_NAME'], '.local')) {
+			if (strstr($_SERVER['SERVER_NAME'], '.local') && $currentlyDeveloping['nexter']) {
 				wp_enqueue_script( 'clubitsolutions-js', 'http://localhost:8080/nexter.js', array(),
 					wp_get_theme()->get
 					( 'Version' ) );
 			} else {
 				wp_enqueue_style( 'nexter-style', get_stylesheet_directory_uri() . '/assets/advanced-css/dist/nexter.css' );
+			}
+		}
+		
+		if ( get_post_field( 'post_name', get_post()) == 'natours') {
+			
+			wp_enqueue_style('lato', '//fonts.googleapis.com/css?family=Lato:100,300,400,700,900');
+			
+			if (strstr($_SERVER['SERVER_NAME'], '.local') && $currentlyDeveloping['natours']) {
+				wp_enqueue_script( 'natours-js', 'http://localhost:8080/natours.js', array(),
+					wp_get_theme()->get
+					( 'Version' ) );
+			} else {
+				wp_enqueue_style( 'natours-style', get_stylesheet_directory_uri() . '/assets/advanced-css/dist/natours.css' );
 			}
 		}
 	}
