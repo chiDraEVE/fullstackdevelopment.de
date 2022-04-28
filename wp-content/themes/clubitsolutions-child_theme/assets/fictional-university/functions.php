586135b1e63a032e9ac01ddd9d24b97497dcdb47
chidraeve <lukas.mainka@clubitsolutions.de>
2022-04-28 16:52:32 +0200

    WPDev, customized most files to render all post-types of the project

--- wp-content/themes/clubitsolutions-child_theme/assets/fictional-university/functions.php
+++ wp-content/themes/clubitsolutions-child_theme/assets/fictional-university/functions.php
@@ -31,53 +31,45 @@ function pageBanner($args = NULL) {
 <?php }
 
 function university_files() {
-  wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
-  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+	wp_enqueue_style( 'fictional-university-style', get_stylesheet_directory_uri() . '/assets/fictional-university/styles.css' );
+	wp_enqueue_script('fictional-university-scripts', get_stylesheet_directory_uri() . '/assets/fictional-university/scripts.js', array('jquery'), '1.0', true);
+	wp_enqueue_style('roboto-font', get_stylesheet_directory_uri() . '/assets/fictional-university/roboto.css');
+	wp_enqueue_style('roboto-condensed-font', get_stylesheet_directory_uri() . '/assets/fictional-university/roboto-condensed.css');
+	wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/assets/fictional-university/fontawesome47/css/font-awesome.min.css');
+	wp_enqueue_script('fictional-university-vendors', get_stylesheet_directory_uri() . '/assets/fictional-university/vendors-scripts.js');
 }
 
-add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features() {
-  add_theme_support('title-tag');
-  add_theme_support('post-thumbnails');
-  add_image_size('professorLandscape', 400, 260, true);
-  add_image_size('professorPortrait', 480, 650, true);
-  add_image_size('pageBanner', 1500, 350, true);
-}
-
-add_action('after_setup_theme', 'university_features');
-
-function university_adjust_queries($query) {
-  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
-    $query->set('orderby', 'title');
-    $query->set('order', 'ASC');
-    $query->set('posts_per_page', -1);
-  }
-
-  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
-    $today = date('Ymd');
-    $query->set('meta_key', 'event_date');
-    $query->set('orderby', 'meta_value_num');
-    $query->set('order', 'ASC');
-    $query->set('meta_query', array(
-              array(
-                'key' => 'event_date',
-                'compare' => '>=',
-                'value' => $today,
-                'type' => 'numeric'
-              )
-            ));
-  }
-}
-
-add_action('pre_get_posts', 'university_adjust_queries');
-
-function universityMapKey($api) {
-  $api['key'] = 'yourKeyGoesHere';
-  return $api;
-}
-
-add_filter('acf/fields/google_map/api', 'universityMapKey');
+//add_action('after_setup_theme', 'university_features');
+//
+//function university_adjust_queries($query) {
+//  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+//    $query->set('orderby', 'title');
+//    $query->set('order', 'ASC');
+//    $query->set('posts_per_page', -1);
+//  }
+//
+//  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+//    $today = date('Ymd');
+//    $query->set('meta_key', 'event_date');
+//    $query->set('orderby', 'meta_value_num');
+//    $query->set('order', 'ASC');
+//    $query->set('meta_query', array(
+//              array(
+//                'key' => 'event_date',
+//                'compare' => '>=',
+//                'value' => $today,
+//                'type' => 'numeric'
+//              )
+//            ));
+//  }
+//}
+//
+//add_action('pre_get_posts', 'university_adjust_queries');
+//
+//function universityMapKey($api) {
+//  $api['key'] = 'yourKeyGoesHere';
+//  return $api;
+//}
+//
+//add_filter('acf/fields/google_map/api', 'universityMapKey');
--- wp-content/themes/clubitsolutions-child_theme/functions.php
+++ wp-content/themes/clubitsolutions-child_theme/functions.php
@@ -1,4 +1,34 @@
 <?php
+	
+	function pageBanner($args = NULL) {
+		
+		if (!$args['title']) {
+			$args['title'] = get_the_title();
+		}
+		
+		if (!$args['subtitle']) {
+			$args['subtitle'] = get_field('page_banner_subtitle');
+		}
+		
+		if (!$args['photo']) {
+			if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
+				$args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+			} else {
+				$args['photo'] = get_stylesheet_directory_uri() . '/assets/fictional-university/images/ocean.jpg';
+			}
+		}
+		
+		?>
+		<div class="page-banner">
+			<div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+			<div class="page-banner__content container container--narrow">
+				<h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+				<div class="page-banner__intro">
+					<p><?php echo $args['subtitle']; ?></p>
+				</div>
+			</div>
+		</div>
+	<?php }
 /**
  * Clubitsolutions-child_theme Theme functions and definitions
  *
@@ -8,6 +38,16 @@
  */
 
 add_action( 'wp_enqueue_scripts', 'clubitsolutions_theme_parent_theme_enqueue_styles' );
+	
+	function university_features() {
+		add_theme_support('title-tag');
+		add_theme_support('post-thumbnails');
+		add_image_size('professorLandscape', 400, 260, true);
+		add_image_size('professorPortrait', 480, 650, true);
+		add_image_size('pageBanner', 1500, 350, true);
+	}
+	
+	add_action('after_setup_theme', 'university_features');
 
 /**
  * Enqueue scripts and styles.
@@ -23,23 +63,39 @@ function clubitsolutions_theme_parent_theme_enqueue_styles() {
 		array( 'clubitsolutions-theme-style' )
 	);
 	
-	if ( is_page( 'fictional-university' ) ) {
+	// Loading assets for Fictional-University-Project
+	$relations = get_field('relations');
+	$isFictionalUniversity = false;
+	$postType = get_post_type();
+	$postTypesOfFictionalUniversity = array("program", "professor", "program", "event", "campus");
+	if ($relations) {
+		foreach ( $relations as $relation ) {
+			$title = get_the_title( $relation );
+			if ( str_contains( $title, 'Fictional University' )
+			     || str_contains( $title, 'Become a WordPress Developer: Unlocking Power With Code'
+			     ) ) {
+				$isFictionalUniversity = true;
+			}
+		}
+	}
+	if (in_array($postType, $postTypesOfFictionalUniversity))
+		$isFictionalUniversity = true;
+	
+	if ( $isFictionalUniversity ) {
 		wp_enqueue_style( 'fictional-university-style', get_stylesheet_directory_uri() . '/assets/fictional-university/styles.css' );
 		wp_enqueue_script('fictional-university-scripts', get_stylesheet_directory_uri() . '/assets/fictional-university/scripts.js', array('jquery'), '1.0', true);
-//		wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 		wp_enqueue_style('roboto-font', get_stylesheet_directory_uri() . '/assets/fictional-university/roboto.css');
-//		wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto:100,300,400,400i,700,700i');
 		wp_enqueue_style('roboto-condensed-font', get_stylesheet_directory_uri() . '/assets/fictional-university/roboto-condensed.css');
-//		wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 		wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/assets/fictional-university/fontawesome47/css/font-awesome.min.css');
 		wp_enqueue_script('fictional-university-vendors', get_stylesheet_directory_uri() . '/assets/fictional-university/vendors-scripts.js');
 	}
 	
-	if ( get_post_type() == 'project') {
+	
+	if ( $postType == 'project') {
 		$currentlyDeveloping = array(
 			'natours' => false,
 			'nexter' => false,
-			'trillo' => true
+			'trillo' => false
 		);
 		
 		if ( get_post_field( 'post_name', get_post()) == 'nexter') {
@@ -81,3 +137,4 @@ function clubitsolutions_theme_parent_theme_enqueue_styles() {
 		}
 	}
 }
+	
