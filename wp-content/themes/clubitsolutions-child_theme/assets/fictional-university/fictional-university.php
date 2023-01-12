1e17b78aec125e5f074498c11309e2b632cf5141
chidraeve <chidraeve@clubitsolutions.de>
2023-01-12 11:30:52 +0100

    moved some code from functions.php to fictional-university.php, solved the issue with php warnings with the isset()-Function and some other few changes

--- wp-content/themes/clubitsolutions-child_theme/assets/fictional-university/fictional-university.php
+++ wp-content/themes/clubitsolutions-child_theme/assets/fictional-university/fictional-university.php
@@ -1,35 +1,4 @@
 <?php
-	function pageBanner($args = NULL) {
-		
-		if (!$args['title']) {
-			$args['title'] = get_the_title();
-		}
-		
-		if (!$args['subtitle']) {
-			$args['subtitle'] = get_field('page_banner_subtitle');
-		}
-		
-		if (!$args['photo']) {
-			if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
-				$args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
-			} else {
-				$args['photo'] = get_stylesheet_directory_uri() . '/assets/fictional-university/images/ocean.jpg';
-			}
-		}
-		
-		?>
-		<div class="page-banner">
-			<div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
-			<div class="page-banner__content container container--narrow">
-				<h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
-				<div class="page-banner__intro">
-					<p><?php echo $args['subtitle']; ?></p>
-				</div>
-			</div>
-		</div>
-				</div>
-
-	<?php }
 	
 	function isFictionalUniversity(): void {
 		global $isFictionalUniversity;
@@ -58,9 +27,56 @@
         if ($isFictionalUniversity) {
             global $projectName;
             $projectName = 'fictional-university';
+            require get_theme_file_path('/inc/like-route.php');
+	        require get_theme_file_path('/inc/search-route.php');
+	
         }
 	}
+ 
+ 
+	function university_custom_rest() {
+	  register_rest_field('post', 'authorName', array(
+	    'get_callback' => function() {return get_the_author();}
+	  ));
+	
+	  register_rest_field('note', 'userNoteCount', array(
+	    'get_callback' => function() {return count_user_posts(get_current_user_id(), 'note');}
+	  ));
+	}
+	
+	add_action('rest_api_init', 'university_custom_rest');
+	function pageBanner($args = NULL) {
+		
+		if (!isset($args['title'])) {
+			$args['title'] = get_the_title();
+		}
+		
+		if (!isset($args['subtitle'])) {
+			$args['subtitle'] = get_field('page_banner_subtitle');
+		}
+		
+		if (!isset($args['photo'])) {
+			if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
+				$args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+			} else {
+				$args['photo'] = get_stylesheet_directory_uri() . '/assets/fictional-university/images/ocean.jpg';
+			}
+		}
+		
+		?>
+		<div class="page-banner">
+			<div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+			<div class="page-banner__content container container--narrow">
+				<h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+				<div class="page-banner__intro">
+					<p><?php echo $args['subtitle']; ?></p>
+				</div>
+			</div>
+		</div>
+				</div>
 
+	<?php }
+	
 	function showUniversityHeader() {
 		?>
 		<div class="fictional-university__header">
@@ -154,23 +170,110 @@
 		</footer>
 	<?php }
 	
-function university_features() {
-	add_theme_support('title-tag');
-	add_theme_support('post-thumbnails');
-	add_image_size('professorLandscape', 400, 260, true);
-	add_image_size('professorPortrait', 480, 650, true);
-	add_image_size('pageBanner', 1500, 350, true);
-}
+    function university_features() {
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_image_size('professorLandscape', 400, 260, true);
+        add_image_size('professorPortrait', 480, 650, true);
+        add_image_size('pageBanner', 1500, 350, true);
+    }
 
-add_action('after_setup_theme', 'university_features');
-
-class PlaceholderBlock {
+    add_action('after_setup_theme', 'university_features');
+    
+    add_action('after_setup_theme', 'university_features');
+	
+	function university_adjust_queries($query) {
+	  if (!is_admin() AND is_post_type_archive('campus') AND $query->is_main_query()) {
+	    $query->set('posts_per_page', -1);
+	  }
+	
+	  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+	    $query->set('orderby', 'title');
+	    $query->set('order', 'ASC');
+	    $query->set('posts_per_page', -1);
+	  }
+	
+	  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+	    $today = date('Ymd');
+	    $query->set('meta_key', 'event_date');
+	    $query->set('orderby', 'meta_value_num');
+	    $query->set('order', 'ASC');
+	    $query->set('meta_query', array(
+	              array(
+	                'key' => 'event_date',
+	                'compare' => '>=',
+	                'value' => $today,
+	                'type' => 'numeric'
+	              )
+	            ));
+	  }
+	}
+	
+	add_action('pre_get_posts', 'university_adjust_queries');
+	
+	function universityMapKey($api) {
+	  $api['key'] = 'yourKeyGoesHere';
+	  return $api;
+	}
+	
+//	add_filter('acf/fields/google_map/api', 'universityMapKey');
+	
+	// Redirect subscriber accounts out of admin and onto homepage
+	add_action('admin_init', 'redirectSubsToFrontend');
+	
+	function redirectSubsToFrontend() {
+	  $ourCurrentUser = wp_get_current_user();
+	
+	  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+	    wp_redirect(site_url('/'));
+	    exit;
+	  }
+	}
+	
+	add_action('wp_loaded', 'noSubsAdminBar');
+	
+	function noSubsAdminBar() {
+	  $ourCurrentUser = wp_get_current_user();
+	
+	  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+	    show_admin_bar(false);
+	  }
+	}
+	
+	// Customize Login Screen
+	add_filter('login_headerurl', 'ourHeaderUrl');
+	
+	function ourHeaderUrl() {
+	  return esc_url(site_url('/'));
+	}
+    
+    // Force note posts to be private
+	add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
+	
+	function makeNotePrivate($data, $postarr) {
+	  if ($data['post_type'] == 'note') {
+	    if(count_user_posts(get_current_user_id(), 'note') > 40 AND !$postarr['ID']) {
+	      die("You have reached your note limit.");
+	    }
+	
+	    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+	    $data['post_title'] = sanitize_text_field($data['post_title']);
+	  }
+	
+	  if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
+	    $data['post_status'] = "private";
+	  }
+	  
+	  return $data;
+	}
+ 
+    class PlaceholderBlock {
 		function __construct($name) {
 			$this->name = $name;
 			add_action('init', [$this, 'onInit']);
 		}
 		
-		function ourRenderCallback($attributes, $content, ) {
+		function ourRenderCallback($attributes, $content ) {
 			ob_start();
 			require get_theme_file_path("/blocks/fictional-university/{$this->name}.php");
 			return ob_get_clean();
@@ -261,4 +364,4 @@ class PlaceholderBlock {
 		);
 	}
 	
-	add_filter('allowed_block_types_all', 'myallowedblocks',10, 2);
+//	add_filter('allowed_block_types_all', 'myallowedblocks',10, 2);
