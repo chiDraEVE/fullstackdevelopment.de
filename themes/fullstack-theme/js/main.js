aa3405fb6d94b427095728cc1aa145891c6e573f
chiDraEVE <chidraeve@clubitsolutions.de>
2024-08-27 19:10:32 +0200

    nebenbei auch mal die main.js korrigiert, dass die Schleife im Video aufhört

--- themes/fullstack-theme/js/main.js
+++ themes/fullstack-theme/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const videoElement = document.querySelector('.wp-block-cover__video-background');
     if (videoElement) {
-        videoElement.loop = false;
+        videoElement.removeAttribute('loop')
     }
 });
