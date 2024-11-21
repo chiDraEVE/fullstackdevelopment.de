2fede5426274c7e210404b27403b0a7884d6bbfd
chiDraEVE <chidraeve@clubitsolutions.de>
2024-11-21 20:26:17 +0100

    modifying block functionality to display currentYear

--- plugins/copyright-date-block/build/index.asset.php
+++ plugins/copyright-date-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '77edc42ae097fb874ef9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '030bbbc384cb481d195a');
