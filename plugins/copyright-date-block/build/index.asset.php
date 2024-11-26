c4fd3f96d061f719446860736837bfc4b92bbc31
chiDraEVE <chidraeve@clubitsolutions.de>
2024-11-26 18:32:37 +0100

    Abhängigkeiten installiert, changed to dynamic rendering

--- plugins/copyright-date-block/build/index.asset.php
+++ plugins/copyright-date-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '53a1dfcccf6f7091cd2d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4c8623cf7374eb435d2b');
