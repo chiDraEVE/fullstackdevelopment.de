00a3d2572b17b60ff44d058e954f02a2a0788c42
chidraeve <chidraeve@clubitsolutions.de>
2025-02-09 02:50:06 +0100

    Problem ist, dass Projekt und Container die Daten nicht synchronisieren

--- plugins/bookstore-block/build/index.asset.php
+++ plugins/bookstore-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '5a6273b7bc243db5920b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '1dae0e6e143b3cfe2295');
