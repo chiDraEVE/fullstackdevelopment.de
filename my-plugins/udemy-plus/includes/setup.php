eb0c7201eec0b1275589b16bc7e44e7033e1ad2e
chidraeve <chidraeve@clubitsolutions.de>
2025-04-13 15:13:34 +0200

    my-themes und my-plugins gelöscht

--- my-plugins/udemy-plus/includes/setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function up_setup_theme() {
-  add_image_size('teamMember', 56, 56, true);
-  add_image_size('opengraph', 1200, 630, true);
-}
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if ! wp user get chidraeve --path=/var/www/html > /dev/null 2>&1; then
-  wp user create chidraeve wordpress@fullstackdevelopment.de --role=administrator --user_pass=and14life --path=/var/www/html
-fi
-
-wp plugin uninstall hello-dolly akismet
-wp plugin install query-monitor advanced-custom-fields --activate
