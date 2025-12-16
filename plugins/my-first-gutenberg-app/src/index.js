ba6159635c46ebc39dc4fbc5ba7246a144b042c7
chidraeve <chidraeve@clubitsolutions.de>
2025-12-16 20:09:29 +0100

    erstes Kapitel abgeschlossen, es werden über wp-data die Titel der Pages gesucht und ausgegeben

--- plugins/my-first-gutenberg-app/src/index.js
+++ plugins/my-first-gutenberg-app/src/index.js
@@ -1,16 +1,69 @@
-import { render } from '@wordpress/element';
+import {SearchControl, Spinner} from '@wordpress/components';
+import { useState, createRoot } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+import { store as coreDataStore } from '@wordpress/core-data';
+import { decodeEntities } from '@wordpress/html-entities';
 
 function MyFirstApp() {
-    return <span>Hello from JavaScript!</span>;
+    const [ searchTerm, setSearchTerm ] = useState( '' );
+
+    const { pages, hasResolved } = useSelect(
+        ( select ) => {
+            const query = {};
+            if ( searchTerm ) {
+                query.search = searchTerm;
+            }
+            const selectorArgs = [ 'postType', 'page', query ];
+            return {
+                pages: select( coreDataStore ).getEntityRecords( ...selectorArgs ),
+                hasResolved:
+                    select( coreDataStore ).hasFinishedResolution( 'getEntityRecords', selectorArgs ),
+            }
+        },
+        [ searchTerm ]
+    );
+
+    return (
+        <div>
+            <SearchControl
+                value={ searchTerm }
+                onChange={ setSearchTerm }
+            />
+            <PagesList hasResolved={ hasResolved } pages={ pages }/>
+        </div>
+    );
 }
 
-window.addEventListener(
-    'load',
-    function () {
-        render(
-            <MyFirstApp />,
-            document.querySelector( '#my-first-gutenberg-app' )
-        );
-    },
-    false
-);
+function PagesList( { hasResolved, pages } ) {
+    if ( !hasResolved ) {
+        return <Spinner/>
+    }
+    if ( !pages?.length ) {
+        return <div>No results</div>
+    }
+
+    return (
+        <table className="wp-list-table widefat fixed striped table-view-list">
+            <thead>
+            <tr>
+                <th>Title</th>
+            </tr>
+            </thead>
+            <tbody>
+            { pages.map( ( page ) => (
+                <tr key={ page.id }>
+                    <td>{ decodeEntities( page.title.rendered ) }</td>
+                </tr>
+            ) ) }
+            </tbody>
+        </table>
+    );
+}
+
+window.addEventListener( 'load', () => {
+    const container = document.querySelector( '#my-first-gutenberg-app' );
+    if ( ! container ) return;
+
+    const root = createRoot( container );
+    root.render( <MyFirstApp /> );
+} );
